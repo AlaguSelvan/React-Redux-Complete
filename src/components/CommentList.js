7c54ac139996dd5dbe6a36adc60f4776b6a56e7c
AlaguSelvan <34230909+AlaguSelvan@users.noreply.github.com>
2018-06-15 09:25:43 +0530

    redux testing

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+class CommentList extends Component{
+    renderComments() {
+        return this.props.comments.map(comment => {
+            return <li key={comment}>{comment}</li>;
+        });
+    }
+    render() {
+        return (
+    <div>                                                   <ul>
+            {this.renderComments()}
+        </ul>                       
+        </div>
+        )}
 
 
-export default () => {
-    return (
-    <div>
-        Comment List a
-    </div>
-)
-} 
+}
+
+function mapStateToProps(state) {
+    return {
+        comments: state.comments
+    };
+}
+
+
+export default connect(mapStateToProps)(CommentList);
--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
+import Root from 'Root'
+
 
 let wrapped;
 
 beforeEach(()=>{
-    wrapped = mount(<CommentBox />);
+    wrapped = mount(
+    <Root>
+        <CommentBox />
+    </Root>
+      );
 });
 
 afterEach(()=>{
@@ -17,11 +23,24 @@ it('has a text area and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 }); 
 
+describe('the text area', () => {
+    beforeEach(()=>{
+        wrapped.find('textarea').simulate('change', {
+            target:{ value: 'new comment' }
+        });
+        wrapped.update();
+    })
+  
 it('has a text area when user has typed in', ()=>{
-    wrapped.find('textarea').simulate('change', {
-        target:{ value: 'new comment' }
-    });
-    wrapped.update();
 
-    
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
+
+it('should be emptied when submitted', ()=>{
+   
+   wrapped.find('form').simulate('submit');
+   wrapped.update();
+   expect(wrapped.find('textarea').prop('value')).toEqual('');
+
+});   
+})
