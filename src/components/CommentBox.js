3ae92f1d2984e58792881207ce5dbd9eedd7cd4b
AlaguSelvan <34230909+AlaguSelvan@users.noreply.github.com>
2018-06-16 09:12:43 +0530

    moxios fake api

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -27,9 +27,12 @@ class CommentBox extends Component {
                 </div>
                 </h4>
             </form>
-            <button onClick={this.props.fetchComments()}>Fetch Comments</button>
+            <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch Comments</button>
         </div>
         )
     }
 }
+
+
+
 export default connect(null, actions)(CommentBox);
